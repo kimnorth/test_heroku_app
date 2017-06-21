b0ee6c1d6530caca7743b366c75a691a30768405
Kim North <kimnorth@Kims-MacBook-Pro.local>
2017-06-21 16:02:21 +0100

    Added constraints to articles for character lengths.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ActiveRecord::Base
-	
+
+	# Adding constraints
+
+	validates :title, presence: true, length: {minimum: 3, maximum: 50}
+	validates :description, presence: true, length: {minimum: 3, maximum: 300}
+
+
+
 end
